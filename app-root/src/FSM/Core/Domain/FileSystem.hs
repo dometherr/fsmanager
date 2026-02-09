a12cb80351a4d2726360fd80df3532e1f410ee7a
Guilherme Herter <nogr.gh@gmail.com>
2026-02-08 21:49:44 -0300

    Adding file system builder and pathEntries domain function

--- app-root/src/FSM/Core/Domain/FileSystem.hs
+++ app-root/src/FSM/Core/Domain/FileSystem.hs
@@ -3,9 +3,12 @@ module FSM.Core.Domain.FileSystem
     , Entry (..)
     , FileSystem (..)
     , Registry
+    , newFileSystem
+    , pathEntries
     ) where
 
 import qualified Data.Map              as M
+import           Data.Maybe            (fromMaybe)
 import           FSM.Core.Domain.Types (Content, Filename, Path)
 
 type Registry = M.Map [Path] Entries
@@ -13,9 +16,23 @@ type Entries  = [Entry]
 
 data Entry
     = File      Filename (Maybe Content)
+    -- ^ File abstraction composed of a name and nullable content
     | Directory Path
+    -- ^ Directory that just has a path
     deriving (Show)
 
 data FileSystem
-    = FileSystem { reg  :: Registry, path :: Path }
+    = FileSystem
+          { reg  :: Registry -- ^ The file system registry
+          , path :: Path     -- ^ The current path of the registry
+          }
     deriving (Show)
+
+newFileSystem :: Path -> FileSystem
+newFileSystem basePath = 
+    FileSystem { reg  = M.fromList [([basePath], mempty)]
+               , path = basePath
+               }
+
+pathEntries :: [Path] -> Registry -> Entries
+pathEntries paths = fromMaybe [] . M.lookup paths
